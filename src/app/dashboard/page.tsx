c061fb642c42f85be5a538d4ec51f3212f5b4795
Anderson Kauer <andersonreuak@gmail.com>
2023-09-09 13:58:53 -0300

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import ProtectedRoute from "@/ProtectedRoute";
+
+export default function Dashboard() {
+  const isAuthenticated = true;
+
+  const handleLogout = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <main className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center mb-4">
+        Seja bem-vindo Admin
+      </h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={handleLogout}
+      >
+        Sair
+      </button>
+    </main>
+  );
+}
